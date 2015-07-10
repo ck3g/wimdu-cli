f9c58fd5dbcee19d0faba1ab9dfe3ab241010f33
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-07-10 15:01:37 +0800

    Extract input fields handler into separate method

--- lib/wimdu/option_handler/new.rb
+++ lib/wimdu/option_handler/new.rb
@@ -5,14 +5,26 @@ module Wimdu
         property = Property.create_uniq
         puts "Starting with new property #{property.slug}."
         puts
-        puts "Title: "
-        title = STDIN.gets.chomp
-        property.update title: title
-        puts "Address: "
-        address = STDIN.gets
-        property.update address: address
+        fields.each do |field_name, _|
+          handle_field_input field_name, property
+        end
 
         true
+      end
+
+      private
+
+      def handle_field_input(field_name, property)
+        puts "#{fields[field_name]}: "
+        data = STDIN.gets.chomp
+        property.update field_name => data
+      end
+
+      def fields
+        {
+          title: 'Title',
+          address: 'Address'
+        }
       end
     end
   end
