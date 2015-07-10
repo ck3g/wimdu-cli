fa7f2f2fad9766d709131323a8c4a048370af6eb
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-07-10 16:13:41 +0800

    Display success message when there is no missing fields

--- lib/wimdu/property_input.rb
+++ lib/wimdu/property_input.rb
@@ -5,19 +5,27 @@ module Wimdu
     end
 
     def perform
-      property.missing_fields.each do |field_name|
-        handle_field_input field_name, property
-      end
+      missing_fields.each { |field_name| handle_field_input field_name }
+      display_success_message
     end
 
     protected
 
     attr_reader :property
 
-    def handle_field_input(field_name, property)
+    def missing_fields
+      property.missing_fields
+    end
+
+    def handle_field_input(field_name)
       puts "#{field_name.to_s.capitalize}: "
       data = STDIN.gets.chomp
       property.update field_name => data
+    end
+
+    def display_success_message
+      return unless missing_fields.empty?
+      puts "Great job! Listing #{property.slug} is complete!"
     end
   end
 end
